f843a65bd0a726987101c939edeadde5883a075f
Po-Hsun <pohsun@infinitiesoft.com>
2015-12-28 10:21:27 +0800

    add smart-table plugin in angularjs

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -2,7 +2,7 @@ angular
 		.module(
 				'exampleApp',
 				[ 'ngRoute', 'ngCookies', 'exampleApp.services', 'formly',
-						'formlyBootstrap', 'ui.bootstrap' ])
+						'formlyBootstrap', 'ui.bootstrap' ,'smart-table'])
 		.config(
 				[
 						'$routeProvider',
@@ -203,7 +203,10 @@ angular
 
 function IndexController($scope, NewsService) {
 
-	$scope.newsEntries = NewsService.query();
+	$scope.rowCollection = NewsService.query();
+	//copy the references (you could clone ie angular.copy but then have to go through a dirty checking for the matches)
+	console.log($scope.rowCollection);
+    $scope.displayedCollection = [].concat($scope.rowCollection);
 
 	$scope.deleteEntry = function(newsEntry) {
 		newsEntry.$remove(function() {
@@ -338,7 +341,119 @@ function EditController($scope, $routeParams, $location, NewsService,
 	};
 };
 
-function CreateController($scope, $location, NewsService) {
+function CreateController($scope, $location, NewsService, formlyVersion) {
+	var vm = this;
+	$scope.vm = vm;
+	vm.onSubmit = onSubmit;
+	vm.author = {
+		name : 'pohsun',
+		url : ''
+	};
+	vm.env = {
+		angularVersion : angular.version.full,
+		formlyVersion : formlyVersion
+	};
+
+	vm.model = {
+		awesome : true
+	};
+	vm.options = {
+		formState : {
+			awesomeIsForced : false
+		}
+	};
+
+	vm.fields = [
+			{
+				key : 'date1',
+				type : 'datepicker',
+				templateOptions : {
+					label : 'Date 1',
+					type : 'text',
+					datepickerPopup : 'dd-MMMM-yyyy'
+				}
+			},
+			{
+				key : 'text',
+				type : 'input',
+				templateOptions : {
+					label : 'Text',
+					placeholder : 'Formly is terrific!',
+					type: 'email',
+					required: true
+				}
+			},
+			{
+				key : 'nested.story',
+				type : 'textarea',
+				templateOptions : {
+					label : 'Some sweet story',
+					placeholder : 'It allows you to build and maintain your forms with the ease of JavaScript :-)',
+					description : ''
+				},
+				expressionProperties : {
+					'templateOptions.focus' : 'formState.awesomeIsForced',
+					'templateOptions.description' : function(viewValue,
+							modelValue, scope) {
+						if (scope.formState.awesomeIsForced) {
+							return 'And look! This field magically got focus!';
+						}
+					}
+				}
+			},
+			{
+				key : 'awesome',
+				type : 'checkbox',
+				templateOptions : {
+					label : ''
+				},
+				expressionProperties : {
+					'templateOptions.disabled' : 'formState.awesomeIsForced',
+					'templateOptions.label' : function(viewValue, modelValue,
+							scope) {
+						if (scope.formState.awesomeIsForced) {
+							return 'Too bad, formly is really awesome...';
+						} else {
+							return 'Is formly totally awesome? (uncheck this and see what happens)';
+						}
+					}
+				}
+			},
+			{
+				key : 'whyNot',
+				type : 'textarea',
+				expressionProperties : {
+					'templateOptions.placeholder' : function(viewValue,
+							modelValue, scope) {
+						if (scope.formState.awesomeIsForced) {
+							return 'Too bad... It really is awesome! Wasn\'t that cool?';
+						} else {
+							return 'Type in here... I dare you';
+						}
+					},
+					'templateOptions.disabled' : 'formState.awesomeIsForced'
+				},
+				hideExpression : 'model.awesome',
+				templateOptions : {
+					label : 'Why Not?',
+					placeholder : 'Type in here... I dare you'
+				},
+				watcher : {
+					listener : function(field, newValue, oldValue, formScope,
+							stopWatching) {
+						if (newValue) {
+							stopWatching();
+							formScope.model.awesome = true;
+							formScope.model.whyNot = undefined;
+							field.hideExpression = null;
+							formScope.options.formState.awesomeIsForced = true;
+						}
+					}
+				}
+			} ];
+	function onSubmit() {
+		alert(JSON.stringify(vm.model), null, 2);
+	}
 
 	$scope.newsEntry = new NewsService();
 
