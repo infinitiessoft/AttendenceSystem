c7ddec1ab50df9987dfdc15080f466c17b0bd2e9
Po-Hsun <pohsun@infinitiesoft.com>
2016-01-07 15:38:12 +0800

    add UserDetailServiceImpl

---
+++ src/main/java/security/EmployeeUserDetail.java
@@ -0,0 +1,88 @@
+package security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import entity.Employee;
+import entity.EmployeeRole;
+
+public class EmployeeUserDetail implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String username;
+	private String password;
+	private Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>(0);
+
+	public EmployeeUserDetail() {
+
+	}
+
+	public EmployeeUserDetail(Employee user) {
+		username = user.getUsername();
+		password = user.getPassword();
+		id = user.getId();
+
+		Set<EmployeeRole> roles = user.getEmployeeRoles();
+		for (EmployeeRole role : roles) {
+			authorities
+					.add(new SimpleGrantedAuthority(role.getRole().getName()));
+		}
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	@XmlTransient
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	@XmlTransient
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	@XmlTransient
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	@XmlTransient
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	@XmlTransient
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	@XmlTransient
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+}
---
+++ src/main/java/service/UserDetailServiceImpl.java
@@ -0,0 +1,30 @@
+package service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import security.EmployeeUserDetail;
+import dao.EmployeeDao;
+import entity.Employee;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	private EmployeeDao employeeDao;
+
+	public UserDetailServiceImpl(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		Employee user = employeeDao.findByUsername(username);
+		if (null == user) {
+			throw new UsernameNotFoundException("The employee with username "
+					+ username + " was not found");
+		}
+		return new EmployeeUserDetail(user);
+	}
+
+}
